1693753a953f3cc548d4a2f0c8b82f274966de46
John Katana <johnkatana624@gmail.com>
2026-04-02 16:46:27 +0300

    todays

--- src/index.js
+++ src/index.js
@@ -3,6 +3,56 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 function App() {
+  const pizzaData = [
+    {
+      id: 1,
+      name: "BBQ Chicken Pizza",
+      ingredients: "Chicken, BBQ Sauce, Onions, Cheese",
+      price: 12.99,
+      photoName: "pizzas/BBQ-chicken.jpg",
+      soldOut: false,
+    },
+    {
+      id: 2,
+      name: "Margherita Pizza",
+      ingredients: "Tomato Sauce, Mozzarella, Basil",
+      price: 10.99,
+      photoName: "pizzas/Italian maguerita.jpg",
+      soldOut: false,
+    },
+    {
+      id: 3,
+      name: "Pepperoni Pizza",
+      ingredients: "Tomato Sauce, Mozzarella, Pepperoni",
+      price: 11.99,
+      photoName: "pizzas/pepperoni.jpg",
+      soldOut: true,
+    },
+    {
+      id: 4,
+      name: "Hawaiian Pizza",
+      ingredients: "Tomato Sauce, Mozzarella, Ham, Pineapple",
+      price: 13.99,
+      photoName: "pizzas/hawaiian.jpg",
+      soldOut: false,
+    },
+    {
+      id: 5,
+      name: "Veggie Pizza",
+      ingredients: "Tomato Sauce, Mozzarella, Bell Peppers, Olives, Onions",
+      price: 11.99,
+      photoName: "pizzas/veggie-supreme.jpg",
+      soldOut: false,
+    },
+    {
+      id: 6,
+      name: "Pesto and Tomato pizza",
+      ingredients: "Tomato sauce, Olives, Onions",
+      price: 9.5,
+      photoName: "pizzas/pesto-tomato.jpg",
+      soldOut: false,
+    },
+  ];
   return (
     <div>
       <Header />
@@ -17,10 +67,21 @@ function Header() {
 function Menu() {
   return (
     <div className="menu">
-      <h2>Our Menu</h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <hr></hr>
+      <h2>OUR MENU</h2>
+      <hr></hr>
+      <Pizza
+        name="BBQ Chicken Pizza"
+        ingredients="Chicken, BBQ Sauce, Onions, Cheese"
+        price={12.99}
+        photoName="pizzas/BBQ-chicken.jpg"
+      />
+      <Pizza
+        name="Margherita Pizza"
+        ingredients="Tomato Sauce, Mozzarella, Basil"
+        price={10.99}
+        photoName="pizzas/Italian maguerita.jpg"
+      />
     </div>
   );
 }
@@ -32,10 +93,15 @@ function Footer() {
   );
 }
 
-function Pizza() {
+function Pizza(props) {
   return (
     <div>
-      <img src="pizzas/BBQ-chicken.jpg" alt="BBQ Chicken Pizza" />
+      <img src={props.photoName} alt={props.name} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>${props.price}</span>
+      </div>
     </div>
   );
 }
